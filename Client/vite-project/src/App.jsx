e1aeefc9d7b9a54005f92b28230f96b0a7f3cd38
nishan.kakadiya <nishan.kakadiya@emvigotech.com>
2025-03-17 12:17:18 +0530

    basic structure of client side application

---
+++ Client/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Product from './Page/Product'
+function App() {
+  
+
+  return (
+    <>
+      <Product />
+    </>
+  )
+}
+
+export default App
---
+++ Client/vite-project/src/Page/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Search_Sort from '../Components/Search_Sort'
+function Product() {
+  return (
+    <div>
+      <Search_Sort />
+    </div>
+  )
+}
+
+export default Product
