67b71f453d8645ef230a106eaf229fca6820fdd8
nishan.kakadiya <nishan.kakadiya@emvigotech.com>
2025-03-17 11:57:50 +0530

    BE api work finish

--- Src/Api/Controller/ProductConroller.js
+++ Src/Api/Controller/ProductConroller.js
@@ -7,8 +7,24 @@ exports.GetProduct = async (req,res) => {
         const page =  Number(req.query.page) || 1 ;
         const limit = Number(req.query.limit) || 10;
         const skip = (page-1)*limit
-        const data =  await product.find().skip(skip).limit(limit)
-        res.send(data)
+
+        const search = req.query.search || ""
+        const sort = req.query.sort || ""
+
+        const query = {
+            $or: [
+                { "name": { $regex: search, $options: 'i' } },
+                { "category": { $regex: search, $options: 'i' } }
+            ]
+        };
+        let data;
+        if (sort) {
+            data = await product.find(query).sort(sort).skip(skip).limit(limit);
+        } else {
+            data = await product.find(query).skip(skip).limit(limit);
+        }
+
+        res.send(data);
     }
     catch{
         console.log("cant find data")
